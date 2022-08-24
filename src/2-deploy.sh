ff12d7b75d7ca9d7246145159fd536f3e3c63ab0
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-08-24 15:53:36 +0100

    Add HUMIO variables / parameters

--- src/2-deploy.sh
+++ src/2-deploy.sh
@@ -5,4 +5,6 @@ cd function
 GOOS=linux go build main.go
 cd ../
 aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
-aws cloudformation deploy --template-file out.yml --stack-name blank-go --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy \
+    --parameter-overrides HumioAddress="${HUMIO_ADDRESS}" HumioToken="${HUMIO_TOKEN}" \
+    --template-file out.yml --stack-name blank-go --capabilities CAPABILITY_NAMED_IAM
