bedb3bf4ba2ea1aec7600713317946ed1415c309
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-08-26 13:26:48 +0100

    Parse CloudTrail files and ship them to humio

--- src/function/main.go
+++ src/function/main.go
@@ -12,7 +12,11 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/humio/cli/api"
+	"github.com/humio/cli/shipper"
 	"log"
+	"net/url"
+	"os"
 )
 
 var awsSession = session.New()
@@ -35,12 +39,16 @@ func callLambda() (string, error) {
 	return string(output), err
 }
 
+type cloudTrailFile struct {
+	Records []json.RawMessage `json:"Records"`
+}
+
 type internalSNSMessage struct {
 	S3Bucket    string   `json:"s3Bucket"`    // S3 bucket name
 	S3ObjectKey []string `json:"s3ObjectKey"` // Path inside S3 bucket
 }
 
-func acquireS3File(ctx context.Context, bucketName, path string) error {
+func pushS3ContentToHumio(ctx context.Context, humio *shipper.LogShipper, bucketName, path string) error {
 	s3client := s3.New(awsSession)
 	r, err := s3client.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(bucketName),
@@ -56,8 +64,52 @@ func acquireS3File(ctx context.Context, bucketName, path string) error {
 		return fmt.Errorf("failed to get S3 result: %w", err)
 	}
 	log.Printf("Successfully read %d bytes from S3 %s", bytesRead, path)
-	log.Printf("Content: %s", buf.String())
+
+	var content cloudTrailFile
+	err = json.Unmarshal(buf.Bytes(), &content)
+	if err != nil {
+		return fmt.Errorf("failed to parse cloud trail file located at %s in %s. Error: %w", path, bucketName, err)
+	}
+	log.Printf("parsed %d records", len(content.Records))
+	log.Printf("first record: %v", string(content.Records[0]))
+	for i, _ := range content.Records {
+		humio.HandleLine(string(content.Records[i]))
+	}
+
 	return nil
+}
+
+func newHumioShipper() (*shipper.LogShipper, error) {
+	config := api.DefaultConfig()
+	address := os.Getenv("HUMIO_ADDRESS")
+	parsedUrl, err := url.Parse(address)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse HUMIO_ADDRESS %s: %w", address, err)
+	}
+	config.Address = parsedUrl
+	config.Token = os.Getenv("HUMIO_TOKEN")
+	config.CACertificatePEM = os.Getenv("HUMIO_CA_CERTIFICATE")
+	config.UserAgent = "AWS Lambda Ingest Function"
+	client := api.NewClient(config)
+	if client == nil {
+		return nil, fmt.Errorf("Could not instantiate new humio client")
+	}
+
+	humioShipper := shipper.LogShipper{
+		APIClient:           client,
+		URL:                 "api/v1/repositories/simon/ingest-messages", // TODO: repo url
+		ParserName:          "json",
+		MaxAttemptsPerBatch: 3,
+		BatchSizeLines:      1,
+		BatchSizeBytes:      1024 * 80,
+		Logger:              log.Printf,
+		// TODO: ingest tags
+	}
+
+	humioShipper.ErrorBehaviour = shipper.ErrorBehaviourPanic
+	humioShipper.Start()
+	// TODO: ensure graceful shutdown
+	return &humioShipper, nil
 }
 
 func handleSNSNotification(ctx context.Context, notification events.SNSEntity) error {
@@ -69,8 +121,13 @@ func handleSNSNotification(ctx context.Context, notification events.SNSEntity) e
 	if err != nil {
 		return errors.New(fmt.Sprintf("Cannot parse Message inside SNSEntity: %s", err))
 	}
+	humioShipper, err := newHumioShipper()
+	if err != nil {
+		return fmt.Errorf("Cannot connect to Humio: %w", err)
+	}
+
 	for _, s3path := range msg.S3ObjectKey {
-		err = acquireS3File(ctx, msg.S3Bucket, s3path)
+		err = pushS3ContentToHumio(ctx, humioShipper, msg.S3Bucket, s3path)
 		if err != nil {
 			return err
 		}
